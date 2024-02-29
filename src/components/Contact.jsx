93d895ffcf9bb8eaf643ee9ea9c52a70ecc63a3a
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-02-29 16:43:16 +0000

    Email fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
           <div className="text-center">
             <p className="text-base font-semibold text-indigo-600">TWO HEADS ARE BETTER THAN ONE</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Having a project idea in mind?</h1>
-            <p className="mt-6 text-base leading-7 text-gray-600">"What actions would you take if you had immediate access to a software expert? Whether you're initiating a new project or simply reaching out to say hello, feel free to connect with me. You can also find me with reach me via email at <a className="underline" href="mailto:kobbyedinamdonald@gmail.com">kobbyedinamdonald@gmail.com</a> </p>
+            <p className="mt-6 text-base leading-7 text-gray-600">"What actions would you take if you had immediate access to a software expert? Whether you're initiating a new project or simply reaching out to say hello, feel free to connect with me. You can also find me with reach me via email <a className="underline" href="mailto:kobbyedinamdonald@gmail.com">kobbyedinamdonald@gmail.com</a> </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               {/* <a
                 href="#"
@@ -21,7 +21,7 @@ export default function Contact() {
               >
                 Go back home
               </a> */}
-              <a href="#" className=" rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold bg-indigo-600 text-white-900">
+              <a  href="mailto:kobbyedinamdonald@gmail.com" className=" rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 text-sm font-semibold bg-indigo-600 text-white-900">
                 Let's Talk <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
